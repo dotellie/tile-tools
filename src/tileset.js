6113b96f22e5931aa4aa581889bb805e18c8a729
Ellie <fagerberg.tobias@gmail.com>
2016-11-22 10:24:51 +0100

    Add tileset tile properties

--- src/tileset.js
+++ src/tileset.js
@@ -1,3 +1,5 @@
+import { PropertyObject } from "./propertyObject";
+
 /**
  * A tileset object for storing arbitrary information about tilesets.
  */
@@ -9,9 +11,10 @@ export class TileSet {
 	 * @param {string} [options.name="Tileset"] - {@link TileSet#name}
 	 * @param {string} [options.type="test"] - {@link TileSet#type}
 	 * @param {string} [options.path] - {@link TileSet#path}
-	 * @param {Map<string, *>} [options.properties] - {@link TileSet@properties}
+	 * @param {Map<string, *>} [options.properties] - {@link TileSet#properties}
+	 * @param {object} [options.tileProperties={}] - Default tile properties for the tileset.
 	 */
-	constructor({ name = "Tileset", type = "test", path, properties }) {
+	constructor({ name = "Tileset", type = "test", path, properties, tileProperties = {} }) {
 		/** The name of the tileset.
 		 * @type {string} */
 		this.name = name;
@@ -27,5 +30,71 @@ export class TileSet {
 		/** Custom properties of the tilemap.
 		 * @type {Map<string, *>} */
 		this.properties = properties || new Map();
+
+		this._tileProperties = {};
+		for (let key of Object.keys(tileProperties)) {
+			key = parseInt(key);
+			if (isNaN(key)) {
+				throw new TypeError("Tile property keys have to be numbers");
+			}
+			const value = tileProperties[key];
+			this._tileProperties[key] = new PropertyObject(value);
+		}
+	}
+
+	/**
+	 * Gets a PropertyObject for a tile ID on the TileSet.
+	 *
+	 * @param {number} id - The tileID to get the PropertyObject for.
+	 *
+	 * @returns {PropertyObject} The PropertyObject beloning to this tile ID.
+	 *
+	 * @throws {TypeError} Throws a TypeError if id isn't a number.
+	 * @throws {RangeError} Throws a RangeError if there isn't a PropertyObject for the ID.
+	 */
+	getTileProperties(id) {
+		if (typeof id !== "number") {
+			throw new TypeError("ID has to be a number");
+		}
+		if (!this.hasTileProperties(id)) {
+			throw new RangeError("There are no properties for tile ID " + id);
+		}
+
+		return this._tileProperties[id];
+	}
+
+	/**
+	 * Creates a PropertyObject if needed and returns it.
+	 *
+	 * @param {number} id - The tileID to create the PropertyObject for.
+	 *
+	 * @returns {?PropertyObject} The created PropertyObject or undefined if it already existed.
+	 *
+	 * @throws {TypeError} Throws a TypeError if id isn't a number.
+	 */
+	createTileProperties(id) {
+		if (typeof id !== "number") {
+			throw new TypeError("ID has to be a number");
+		}
+		if (!this.hasTileProperties(id)) {
+			this._tileProperties[id] = new PropertyObject();
+			return this._tileProperties[id];
+		}
+	}
+
+	/**
+	 * Checks if a PropertyObject exists for an ID.
+	 *
+	 * @param {number} id - The tileID to check for a PropertyObject.
+	 *
+	 * @returns {boolean}
+	 *
+	 * @throws {TypeError} Throws a TypeError if id isn't a number.
+	 */
+	hasTileProperties(id) {
+		if (typeof id !== "number") {
+			throw new TypeError("ID has to be a number");
+		}
+		return typeof this._tileProperties[id] !== "undefined";
 	}
 }
--- test/tileset.js
+++ test/tileset.js
@@ -23,5 +23,78 @@ describe("TileSet", () => {
 			tileset.type.should.equal(type);
 			tileset.path.should.equal(path);
 		});
+		it("fails if any tile properties key isn't a number", () => {
+			(() => {
+				// eslint-disable-next-line no-new
+				new TileSet({ tileProperties: {
+					"a string": "should be invalid"
+				} });
+			}).should.throw(TypeError);
+		});
+	});
+
+	/** @test {TileSet#getTileProperties} */
+	describe("#getTileProperty", () => {
+		let tileset;
+
+		beforeEach(() => {
+			tileset = new TileSet({
+				tileProperties: {
+					"0": [["test", "isTest"]],
+					"5": [["test5", "is5"]]
+				}
+			});
+		});
+
+		it("gets a property object correctly", () => {
+			tileset.getTileProperties(0).get("test").should.equal("isTest");
+			tileset.getTileProperties(5).get("test5").should.equal("is5");
+		});
+
+		it("throws an error if ID doesn't have a properties object", () => {
+			(() => {
+				tileset.getTileProperties(10);
+			}).should.throw(RangeError);
+		});
+
+		it("fails if argument isn't a number", () => {
+			(() => {
+				tileset.getTileProperties("a string");
+			}).should.throw(TypeError);
+		});
+	});
+
+	/** @test {TileSet#createTileProperties} */
+	describe("#createTileProperties", () => {
+		it("creates a tile property for a tile ID", () => {
+			const tileset = new TileSet({});
+			tileset.createTileProperties(2);
+			tileset.hasTileProperties(2).should.be.ok;
+		});
+
+		it("fails if argument isn't a number", () => {
+			(() => {
+				new TileSet({}).createTileProperties("a string");
+			}).should.throw(TypeError);
+		});
+	});
+
+	/** @test {TileSet#hasTileProperties} */
+	describe("#hasTileProperties", () => {
+		it("checks if there is a tile property object for an ID", () => {
+			const tileset = new TileSet({
+				tileProperties: {
+					"4": [["key", "value"]]
+				}
+			});
+			tileset.hasTileProperties(4).should.be.ok;
+			tileset.hasTileProperties(0).should.not.be.ok;
+		});
+
+		it("fails if argument isn't a number", () => {
+			(() => {
+				new TileSet({}).hasTileProperties("a string");
+			}).should.throw(TypeError);
+		});
 	});
 });
